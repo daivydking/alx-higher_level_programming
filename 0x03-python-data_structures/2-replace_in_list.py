c34555bb1b8d2510ea168f7ee9cd37847d10d101
daivydking <radchibuike@gmail.com>
2024-01-22 13:43:10 -0800

    replace element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return(my_list)
+    
+    ele = len(my_list)
+
+    if idx > ele - 1:
+        return(my_list)
+    
+    my_list[idx] = span
+
+    return (my_list)
