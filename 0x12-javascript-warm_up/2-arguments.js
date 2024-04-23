cdf464875d41b20b786de07ea62a71eccdcd6444
root <you@example.com>
2024-04-22 21:49:07 -0700

    Js-

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
+const { argv } = require('process');
+const argc = argv.length;
+
 if (process.argv.length === 3) {
-    console.log("No argument");
+    console.log('No argument');
 } else if (process.argv.length === 4) {
-    console.log("Argument found");
+    console.log('Argument found');
 } else {
-    console.log("Arguments found");
+    console.log('Arguments found');
 }
