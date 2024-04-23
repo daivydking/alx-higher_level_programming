a4df428cdba83287861b4a979797db084e0f6d76
root <you@example.com>
2024-04-22 21:45:21 -0700

    JS

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-cosole.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+cosole.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
