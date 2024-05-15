7b8a0e3ca88c4f825d12ebdf822664cbc5d97829
daivydking <radchibuike@gmail.com>
2024-05-14 21:25:18 -0700

    p-network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Fetches https://alx-intranet.hbtn.io/status"""
-import urlib.request
+import urllib.request
+
 
 if __name__ == "__main__":
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
